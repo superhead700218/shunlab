04ea47da24d1ef61d20fc1b1d8dc642ee6f2b738
superhead700218 <57740009+superhead700218@users.noreply.github.com>
2019-11-19 17:43:51 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+yum -y install yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
 yum install -y --setopt=obsoletes=0 docker-ce-17.03.1.ce-1.el7.centos docker-ce-selinux-17.03.1.ce-1.el7.centos
 systemctl start docker
