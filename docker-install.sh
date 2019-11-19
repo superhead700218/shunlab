f3cf521bf1b4ac9ec591a8c099e5cc8b731b17b3
superhead700218 <57740009+superhead700218@users.noreply.github.com>
2019-11-19 17:40:10 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,10 +1,6 @@
 #/bin/bash
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-
 yum install -y --setopt=obsoletes=0 docker-ce-17.03.1.ce-1.el7.centos docker-ce-selinux-17.03.1.ce-1.el7.centos
-
 systemctl start docker
-
 systemctl enable docker
-
 usermod -aG docker pentium
